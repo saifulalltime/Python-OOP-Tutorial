64b82986fe3d197250ce7a2d8ba79490f8b5e9a9
Saiful Islam <saifulalltime@gmail.com>
2024-03-27 00:07:57 +0600

    - Variable and data type and string

---
+++ BasicPython/string_condition.py
@@ -0,0 +1,14 @@
+# String - String is data type that stores a sequence of characters
+# Basic Operations
+# Concatenation
+
+str1 = "Saiful"
+str2 = "Islam"
+print(str1 + " " + str2)
+print(len(str1))
+
+# Indexing
+
+str3 = "Saiful"
+
+print(str3[0])
---
+++ BasicPython/variable_data_type.py
@@ -0,0 +1,37 @@
+# Variables - A variable is a name given to a memory location in a program
+name = "Saiful"  # = Assignment operator
+age = 23
+study = "CSE"
+old = True
+a = None
+
+print("My name: ", name)
+print("My age: ", age,"\nStudy: ", study)
+
+
+# Check types of variables
+print(type(name))
+print(type(a))
+print(type(old))
+
+# Data types
+# integer (25, -25, 0), string, float, boolean (True, False), None
+
+# Keywords - keywords are reserved words in Python.
+"""
+and, as, assert, break, continue, def, del, elif, else, like this lots of keywords
+"""
+#  Type casting
+a = int("4")
+a = float(4.78)
+
+# Input 
+name = input("Enter your name: ")
+age = int(input("Age: "))
+marks = float(input("Marks: "))
+
+print("Welcome",name)
+print("Age",age)
+print("Marks",marks)
+
+
